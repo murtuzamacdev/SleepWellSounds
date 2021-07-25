23cbc22e409ba59996bd33b9d28d8c3612bc6cdb
Murtuza Chakkiwala <>
2021-07-25 16:20:07 +0530

    edit fav screen

--- App.js
+++ App.js
@@ -81,6 +81,33 @@ const AppWrapper = () => {
 
   }, []);
 
+  const onTapTemp = (selectedSound) => {
+    var Sound = require('react-native-sound');
+
+    // Enable playback in silence mode
+Sound.setCategory('Playback');
+
+// Load the sound file 'whoosh.mp3' from the app bundle
+// See notes below about preloading sounds within initialization code below.
+var whoosh = new Sound('whoosh.mp3', Sound.MAIN_BUNDLE, (error) => {
+  if (error) {
+    console.log('failed to load the sound', error);
+    return;
+  }
+  // loaded successfully
+  console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
+
+  // Play the sound with an onEnd callback
+  whoosh.play((success) => {
+    if (success) {
+      console.log('successfully finished playing');
+    } else {
+      console.log('playback failed due to audio decoding errors');
+    }
+  });
+});
+  }
+
   const onTap = (selectedSound) => {
     if (selectedSound.player) { // If already playing
       soundContext.removeSound(selectedSound);
